50e566f076e609fe9ed96662f0d7beed3bb3785a
Taline da Costa <talinedacosta@gmail.com>
2023-01-13 14:32:49 -0300

    style(theme): add theme config

---
+++ src/theme/fonts.js
@@ -0,0 +1,6 @@
+const fonts = {
+    heading: `'Karla', sans-serif`,
+    body: `'Markazi Text', serif`,
+}
+
+export default fonts;
---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react';
+import styles from './style';
+import fonts from './fonts';
+import { brand } from './colors'
+
+const overrides = {
+    styles,
+    fonts: {
+        fonts
+    },
+    colors: {
+        brand
+    }
+}
+
+const theme = extendTheme(overrides)
+export default theme;
