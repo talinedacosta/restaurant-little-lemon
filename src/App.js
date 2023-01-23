3f8261747b215e9415a5421f9ce6d5203d5d01f2
Taline <talinedacosta@gmail.com>
2023-01-23 11:43:14 -0300

    cleanup(component): remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import '@fontsource/karla/400.css'
 import '@fontsource/markazi-text/500.css'
